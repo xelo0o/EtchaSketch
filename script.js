db09b7441961068ebfe590c9577fe7e0126901b6
Mike M. <millerdev17@gmail.com>
2024-03-06 13:09:24 -0500

    create grid size button functionality

--- script.js
+++ script.js
@@ -2,15 +2,62 @@
 const container = document.querySelector('.container');
 const gridSize = document.querySelector('#gridSize');
 const gridSizeOutput = document.querySelector('#gridSizeOutput');
+const cellContainer = document.querySelector('#cellContainer');
+const gridCell = document.querySelector('.gridCell');
+const createGridBtn = document.querySelector('#createGridBtn');
 
-// create
-const div = document.createElement('div');
+
+
+
+// Creates grid based on slider values
+function grid(){      
+
+    //create grid (x * x)
+let num = gridSize.value * gridSize.value;
+//get percentage for cell W * H;
+let size = ((700 / gridSize.value) / 700) *100;    
+
 
 //sets output text to value of slider.
 gridSizeOutput.textContent = `${gridSize.value} x ${gridSize.value}`;
 //updates value when you move slider
 gridSize.oninput = function(){
+    // update gridSize.value attribute
+    this.setAttribute('value', this.value)
+    //update output text
     gridSizeOutput.textContent = `${gridSize.value} x ${gridSize.value}`;
 }
 
-console.log(gridSize.value)
+
+
+
+    for(let i = 0; i < num; i++){
+        const div = document.createElement('div');
+        div.style.width = `${size}%`;
+        div.style.height = `${size}%`;
+        div.classList.add('gridCell');
+        cellContainer.appendChild(div);
+    }
+}
+
+
+
+function clearGrid(){
+    while(cellContainer.firstChild){
+        cellContainer.removeChild(cellContainer.firstChild); 
+    }
+}
+
+// EVENT LISTENERS
+
+createGridBtn.addEventListener('click',()=>{
+    clearGrid();
+    grid();
+});
+
+
+
+// call grid to create first 16x16 grid
+grid();
+
+
