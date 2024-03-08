b632aaeed3f25894a6c9f2ff19d714a10e9e516e
Mike M. <millerdev17@gmail.com>
2024-03-08 12:40:05 -0500

    added drawing and erasing functionality

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const gridSizeOutput = document.querySelector('#gridSizeOutput');
 const cellContainer = document.querySelector('#cellContainer');
 const gridCell = document.querySelector('.gridCell');
 const createGridBtn = document.querySelector('#createGridBtn');
+const eraseBtn = document.querySelector('#eraseBtn');
 
 
 
@@ -15,9 +16,7 @@ function grid(){
     //create grid (x * x)
 let num = gridSize.value * gridSize.value;
 //get percentage for cell W * H;
-let size = ((700 / gridSize.value) / 700) *100;    
-
-
+let size = ((700 / gridSize.value) / 700) *100;   
 //sets output text to value of slider.
 gridSizeOutput.textContent = `${gridSize.value} x ${gridSize.value}`;
 //updates value when you move slider
@@ -29,23 +28,44 @@ gridSize.oninput = function(){
 }
 
 
-
-
+// creates the gridCells
     for(let i = 0; i < num; i++){
         const div = document.createElement('div');
         div.style.width = `${size}%`;
         div.style.height = `${size}%`;
+        div.setAttribute('id', 'cell');
         div.classList.add('gridCell');
         cellContainer.appendChild(div);
     }
+
+    black();
 }
 
-
-
+// clear all children from container
 function clearGrid(){
     while(cellContainer.firstChild){
         cellContainer.removeChild(cellContainer.firstChild); 
     }
+}
+// change background to black
+function black() {
+    const gridCellAll = document.querySelectorAll('.gridCell');
+    gridCellAll.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "black";
+            
+        });
+    });
+}
+// change background back to white
+function erase(){
+    const gridCellAll = document.querySelectorAll('.gridCell');
+    gridCellAll.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "#FFF";
+            
+        });
+    });
 }
 
 // EVENT LISTENERS
@@ -55,9 +75,15 @@ createGridBtn.addEventListener('click',()=>{
     grid();
 });
 
+eraseBtn.addEventListener('click', ()=>{
+    erase();
+})
 
+
+
+//eventlistener to change background
 
 // call grid to create first 16x16 grid
-grid();
 
+grid();
 
